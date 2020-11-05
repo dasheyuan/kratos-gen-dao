877841ec98387c91739f5cb42abc6be4db9511f3
chenyuan <dasheyuan@qq.com>
2020-11-05 18:00:21 +0800

    mod modify go.mod

--- internal/schema/config.go
+++ internal/schema/config.go
@@ -2,6 +2,7 @@ package schema
 
 import (
 	"database/sql"
+	"fmt"
 	"io"
 )
 
@@ -9,6 +10,10 @@ import (
 func GetSchema(w io.Writer, db *sql.DB, tableName, dbName string) (string, error) {
 	cols, err := readTableStruct(db, tableName, dbName)
 	if nil != err {
+		return "", err
+	}
+	if len(cols) == 0 {
+		err = fmt.Errorf("Information not found, Check your params?")
 		return "", err
 	}
 	r, structName, err := createStructSourceCode(cols, tableName)
